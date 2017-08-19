165b2a41c50ae9118c5ab4835b26a7e506ca7510
rayGreycloud <rayGreycloud@gmail.com>
2017-08-18 19:08:33 -0700

    Add axios and api call for data

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({
+        albums: response.data
+      }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
   render() {
+    console.log(this.state);
     return (
       <View>
-        <Text>Album List</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
